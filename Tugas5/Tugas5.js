fbf2e81e7a475052213fddca0551fbafefd7bf23
vitaqila <vrwy30998@gmail.com>
2025-03-23 12:23:54 +0700

    Update Tugas5.js

--- Tugas5/Tugas5.js
+++ Tugas5/Tugas5.js
@@ -2,9 +2,9 @@
 let produklist = [
     { id: 1, nama: "Laptop", harga: 12000000 },
     { id: 2, nama: "Smartphone", harga: 5000000 },
-    { id: 3, nama: "Tablet", harga: 7000000 }, // Menambahkan Produk
-    { id: 4, nama: "Headphone", harga: 1500000 }, // Menambahkan Produk
-    { id: 5, nama: "Smartwatch", harga: 3000000 } // Menambahkan Produk
+    { id: 3, nama: "Tablet", harga: 7000000 }, 
+    { id: 4, nama: "Headphone", harga: 1500000 }, 
+    { id: 5, nama: "Smartwatch", harga: 3000000 } 
 ];
 
 const eventHandler = {
